822e706e64619330b5713f766ee3a95c158f7298
raoulparmesan <raoulparmesan@gmail.com>
2025-12-04 12:50:59 +0100

    Update SimpleForm.tsx

--- components/SimpleForm.tsx
+++ components/SimpleForm.tsx
@@ -228,4 +228,4 @@ export default function SimpleForm() {
 
     </div>
   );
-                                                                   }
+                                                                   
